d9112b0ea661173dd7827257c97fe86677a63475
Hayk Zakaryan <zakaryanhayk@Hayks-MacBook-Pro.local>
2024-07-14 03:33:34 +0400

    Fixing logging issues

--- src/main/java/com/haykz/Main.java
+++ src/main/java/com/haykz/Main.java
@@ -11,13 +11,16 @@ public class Main {
             //Creating a producer and sending a message to the topic(test-topic)
             KafkaMessageProducer producer = new KafkaMessageProducer();
             producer.sendMessage("test-topic", "text", "Hello, Kafka!");
+            producer.sendMessage("test-topic", "text", "Text1");
+            producer.sendMessage("test-topic", "text", "Text2");
             producer.close();
 
             //Creating a Writer service to consume all the messages and write them in the db.
+            //Enter your DB user and password here
             MessageWriterService service = new MessageWriterService(
                     "test-topic",
                     "jdbc:postgresql://localhost:5432/test_database",
-                    "db_username",
+                    "db_user",
                     "db_password"
             );
             service.start();
